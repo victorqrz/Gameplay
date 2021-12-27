15afd40a4d0bd69e92acca296d5dca491ced8771
Victor Queiroz <victoorqrz@gmail.com>
2021-12-27 18:30:17 -0300

    setting up fonts and refactoring components

--- App.tsx
+++ App.tsx
@@ -1,4 +1,35 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { useFonts } from 'expo-font';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import {
+  Rajdhani_500Medium,
+  Rajdhani_700Bold,
+} from '@expo-google-fonts/rajdhani';
+import AppLoading from 'expo-app-loading';
+
 import { SingIn } from './src/screens/SignIn';
+
 export default function App() {
-  return <SingIn />;
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <SingIn />
+    </>
+  );
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Image, StatusBar } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 import { ButtonIcon } from '../../components/ButtonIcon';
 import IllustrationImg from '../../assets/illustration.png';
@@ -8,23 +8,17 @@ import { styles } from './styles';
 export function SingIn() {
   return (
     <View style={styles.container}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
       <Image
         source={IllustrationImg}
         style={styles.image}
         resizeMode="stretch"
       />
-
       <View style={styles.content}>
         <Text style={styles.title}>
-          Organize{`\n`}suas jogatinas{`\n`}facilmente
+          Conecte-se{`\n`}e organize suas{'\n'}jogatinas
         </Text>
         <Text style={styles.subtitle}>
-          Crie grupos para jogar seus games{`\n`}favoritos com seu amigos
+          Crie grupos para jogar seus games{'\n'}favoritos com seu amigos
         </Text>
         <ButtonIcon title="Entrar com Discord" activeOpacity={0.7} />
       </View>
